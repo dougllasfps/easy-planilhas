4fb30131155418ad4f9b5cead2bf02ae7ab6b432
Dougllas Sousa <dougllas.sousa@seduc.ce.gov.br>
2017-01-13 08:58:30 -0300

    Developing new Model

---
+++ src/main/java/org/dougllas/planilhas/model/Row.java
@@ -0,0 +1,26 @@
+package org.dougllas.planilhas.model;
+
+/**
+ * Criado por dougllas.sousa em 13/01/2017.
+ */
+public class Row {
+
+    private int index;
+    private Object[] data;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Object[] getData() {
+        return data;
+    }
+
+    public void setData(Object[] data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/org/dougllas/planilhas/model/SpreadSheet.java
@@ -0,0 +1,16 @@
+package org.dougllas.planilhas.model;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Criado por dougllas.sousa em 13/01/2017.
+ */
+public class SpreadSheet<T> {
+
+    private Collection<T> data;
+    private List<Column> columns;
+    private List<Row> rows;
+
+    
+}
